b5f223081cc585f16f82b9eff50c97793025a3c5
Ruben Villegas <ruben_villegas@yeah.net>
2024-07-06 14:27:16 +0900

    Create fun_print_diamond.py

---
+++ fun_print_diamond.py
@@ -0,0 +1,8 @@
+# fun_print_diamond.py
+def print_diamond(n):
+    for i in range(n):
+        print(' ' * (n - i - 1) + '*' * (2 * i + 1))
+    for i in range(n - 2, -1, -1):
+        print(' ' * (n - i - 1) + '*' * (2 * i + 1))
+
+print_diamond(5)
