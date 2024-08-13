ea350076958169615e21cb5d7612d12149322c7c
Ruben Villegas <ruben_villegas@yeah.net>
2024-08-13 17:52:55 +0900

    Create func_py_calculate_hexagon_area.py

---
+++ func_py_calculate_hexagon_area.py
@@ -0,0 +1,7 @@
+# func_py_calculate_hexagon_area.py
+import math
+
+def func_py_calculate_hexagon_area(side_length):
+    return (3 * math.sqrt(3) * side_length**2) / 2
+
+print(func_py_calculate_hexagon_area(5))
