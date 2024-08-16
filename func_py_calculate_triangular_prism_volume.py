3d9e43e765c8cca067b02b4ece00ddffe76fdf03
Ruben Villegas <ruben_villegas@yeah.net>
2024-08-16 21:36:49 +0900

    Create func_py_calculate_triangular_prism_volume.py

---
+++ func_py_calculate_triangular_prism_volume.py
@@ -0,0 +1,6 @@
+# func_py_calculate_triangular_prism_volume.py
+def func_py_calculate_triangular_prism_volume(base, height, length):
+    area = (base * height) / 2
+    return area * length
+
+print(func_py_calculate_triangular_prism_volume(4, 3, 10))
