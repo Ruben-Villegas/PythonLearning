16b69d40c13b477ee72bd6b866cd6aff6ba26bd9
Ruben Villegas <ruben_villegas@yeah.net>
2024-08-25 22:33:34 +0900

    Create func_py_calculate_trapezoid_area.py

---
+++ func_py_calculate_trapezoid_area.py
@@ -0,0 +1,5 @@
+# func_py_calculate_trapezoid_area.py
+def func_py_calculate_trapezoid_area(base1, base2, height):
+    return 0.5 * (base1 + base2) * height
+
+print(func_py_calculate_trapezoid_area(4, 6, 5))
