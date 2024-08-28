53696483b88c6069ec2b8b9176da45512cdb174a
Ruben Villegas <ruben_villegas@yeah.net>
2024-08-28 22:04:39 +0900

    Create func_py_calculate_paraboloid_volume.py

---
+++ func_py_calculate_paraboloid_volume.py
@@ -0,0 +1,7 @@
+# func_py_calculate_paraboloid_volume.py
+import math
+
+def func_py_calculate_paraboloid_volume(radius, height):
+    return (1/2) * math.pi * radius**2 * height
+
+print(func_py_calculate_paraboloid_volume(4, 7))
