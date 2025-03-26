14c6b19a66c723fbfb99630e4d90e36334e02269
Ruben Villegas <ruben_villegas@yeah.net>
2025-03-26 17:11:31 +0800

    Create fun_py_calculate_exponential.py

---
+++ part2/fun_py_calculate_exponential.py
@@ -0,0 +1,10 @@
+# fun_py_calculate_exponential.py
+
+def fun_py_calculate_exponential(base, exp):
+    return base ** exp
+
+def test_calculate_exponential():
+    print(f"2^5: {fun_py_calculate_exponential(2, 5)}")
+
+if __name__ == "__main__":
+    test_calculate_exponential()
